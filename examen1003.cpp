ff4bd361399d5f5283610e5924e423f128fd01dc
facfacfac <fernando.agustin@itgranada.com>
2022-03-10 09:56:18 +0100

    probandoVIP

--- examen1003.cpp
+++ examen1003.cpp
@@ -8,12 +8,16 @@ private:
 string nombre;
 string DNI;
 int edad;
+bool VIP;
+
+
 public:
   // Constructor.
-  Persona (string nombre, string DNI, int edad = 18) {
+  Persona (string nombre, string DNI, int edad = 18, bool VIP = false) {
     setNombre(nombre);
     setDNI(DNI);
     setEdad(edad);
+    calcularVIP();
   }
   // Setters and getters
   void setNombre(string nombre) {
@@ -36,11 +40,38 @@ public:
   }
   // Imprimir
   void imprimir() {
-    cout << "El nombre de la persona es " << this->nombre << ", su DNI es " << this->DNI << " y su edad es " << this->edad << " años." << endl;
+    cout << "El nombre de la persona es " << this->nombre << ", su DNI es " << this->DNI << " y su edad es " << this->edad << " años." << " La persona es VIP: " << this->VIP << endl;
   }
   // Métodos varios.
   bool mayorEdad(){
     return this->edad>=18;
+  }
+  calcularVIP() {
+    bool VIP = false;
+    int numer1 = 0;
+    int numero2 = 0;
+    int numero3 = 0;
+    int numero4 = 0;
+    int numero5 = 0;
+    cout << "Introduce tres numeros:" << endl;
+    cout << "Numero 1: "
+    cin >> numero1;
+    cout << "Numero 2: "
+    cin >> numero2;
+    cout << "Numero 3: "
+    cin >> numero3;
+    if ()(numero1+numero2+numero3)%11 == 0) {
+      VIP = true;
+    } else if ((numero1+numero2+numero3%11 != 0) && (numero1 == 13 || numero 2 == 13 numero = || 13) {
+      cout << "Puedes introducir dos números más:" << endl;
+      cout << "Numero 4: "
+      cin >> Numero4;
+      cout << "Numero 5: "
+      cin >> Numero 5;
+      if ((numero1+numero2+numero3+numero4+numero5)%11 == 0) {
+        VIP = true;
+      }
+    }
   }
 
 };
@@ -49,8 +80,18 @@ public:
 int main(){
   Persona fulanito("Fulano","44844848A", 30);
   fulanito.imprimir();
+  int numero;
+  vector<int> numerosVIP;
 
   cout << "La persona " << fulanito.getNombre() << "es mayor de edad: " << fulanito.mayorEdad() << endl;
+
+  cout << "Operacion para saber si la persona" << fulanito.getNombre() << "es VIP: introduce tres numeros." << endl;
+  cout << "Introduce el primer numero: "
+  cin << numero;
+  numerosVIP.push_back(numero);
+  cout << endl;
+  cout << "Introduce el primer numero: "
+
 
 
 
